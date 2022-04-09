0f8420e5fe8a83c96738188a8d933546ab97063a
kalikal1 <kalkidantiru@gmail.com>
2022-04-08 18:00:01 -0700

    this code prints an output using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#include<stdio>
+#include <stdio.h>
 
 /**
-  *main -Entry point
-  *
-  * Return: Always 0 (success)
-  *
-  */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	retun(0);
+	return (0);
 }
