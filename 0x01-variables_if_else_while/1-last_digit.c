79d4755156f94f27406a2eb79c4f9604d3a1a150
kalikal1 <kalkidantiru14@gmail.com>
2022-04-11 01:34:39 +0300

    this coe is going to print the last difit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
  * main - Prints a text according number
  *
@@ -7,22 +9,24 @@
  */
 int main(void)
 {
-	int n;
+	int n, lastd;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-        int lastd =n %10;
-         if(lastd>5)
-          {
-          printf("Last digit of %d is %d and is greater than 5\n",n,lastd);
-          }
-         else if(lastd==0)
-          {
-          printf("Last digit of %d is %d and is 0\n",n, lastd);
-          }
-         else if(lastd<6 && lastd !=0)
-          {
-          printf("Last digit of %d is %d and is less than 6 and is not 0\n",n, lastd);
-          };
+	lastd = n % 10;
+
+	if (lastd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastd);
+	}
+	else if (lastd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lastd);
+	}
+	else if (lastd < 6 && lastd != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
+	}
+
 	return (0);
 }
