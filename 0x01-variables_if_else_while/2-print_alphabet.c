c47d3abcd034166a8b58b989f5f27dff522e07c0
kalikal1 <kalkidantiru14@gmail.com>
2022-04-11 01:44:37 +0300

    this code prints alphabetes

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+char a;
+for(a='a';a<='z'; a++)
+putchar(a);
+putchar('\n');
+return(0);
+}
