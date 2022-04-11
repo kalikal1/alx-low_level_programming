6fda43bd33b27eb171de35a397e3a39ffe12f18c
kalikal1 <kalkidantiru14@gmail.com>
2022-04-11 04:13:18 +0300

    this code prints numers separated by comma and space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+  * main - Prints a serie of numbers with commas
+  *
+  * Return: Always (Success);
+  */
+int main(void)
+{
+	int a;
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		
+ 		if (c !=9);
+
+ 		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+		
+
+
